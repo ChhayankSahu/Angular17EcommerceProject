f2c4abc41c8fc5ea303c5db0aa25010f05ab1117
ChhayankSahu <sahu.chhayank5@gmail.com>
2024-07-26 15:38:12 +0530

    Admin Dashboard componant updated.

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,12 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../services/admin.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit {
+
+  user_dashboard_data: any;
+  total_user_number: number = 0;
+  admin_user_number: number = 0;
+  seller_user_number: number = 0;
+  buyer_user_number: number = 0;
+
+  product_dashboard_data: any;
+  total_product_number: number = 0;
+  published_product_number: number = 0;
+  inactive_product_number: number = 0;
+  draft_product_number: number = 0;
+
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+    this.adminUserDashboardData();
+    this.adminProductDashboardData();
+
+
+  }
+  constructor(private adminService: AdminService, private router: Router) {
+
+  }
+
+
+  user_dashboard() {
+    this.router.navigateByUrl("/admin/user");
+  }
+  product_dashboard() {
+    this.router.navigateByUrl("/admin/product");
+  }
+  adminUserDashboardData() {
+    this.adminService.allUser().subscribe(
+      data => {
+        this.user_dashboard_data = data;
+        console.log(this.user_dashboard_data);
+        for (let user in this.user_dashboard_data) {
+          if (this.user_dashboard_data[user].role == "admin") { ++this.admin_user_number; }
+          else if (this.user_dashboard_data[user].role == "seller") { ++this.seller_user_number; }
+          else if (this.user_dashboard_data[user].role == "buyer") { ++this.buyer_user_number; }
+          ++this.total_user_number;
+        }
+      }, error => { console.log("myerror", error); }
+    );
+  }
+
+  adminProductDashboardData() {
+    this.adminService.prodoctDashboardData().subscribe(
+      data => {
+        this.product_dashboard_data = data;
+        console.log(this.product_dashboard_data);
+        for (let status in this.product_dashboard_data) {
+          if (this.product_dashboard_data[status].status == "publish") { ++this.published_product_number; }
+          else if (this.product_dashboard_data[status].status == "inactive") { ++this.inactive_product_number; }
+          else if (this.product_dashboard_data[status].status == "draft") { ++this.draft_product_number; }
+          ++this.total_product_number;
+        }
+      }, error => { console.log("myerror", error); }
+    );
+  }
+
+
 
 }
