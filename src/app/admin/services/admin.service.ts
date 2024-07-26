2ae6c9fd901a75da8aeca460203c602e5997d9f6
ChhayankSahu <sahu.chhayank5@gmail.com>
2024-07-26 13:01:35 +0530

    header optons updated, options functions created in adminService

--- src/app/admin/services/admin.service.ts
+++ src/app/admin/services/admin.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../../core/service/api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
+  public user_url = "http://localhost:3000/user/";//for single user bby userid
+  public product_url = "http://localhost:3000/products/";
+  public orders_url = "http://localhost:3000/orders/";
+  public all_users_url = "http://localhost:3000/users/";//for all users
 
-  constructor() { }
+  constructor(private apiService:ApiService) { }
+
+  userDashboardData()
+{
+return this.apiService.get(this.user_url);
+}
+prodoctDashboardData()
+{
+  return this.apiService.get(this.product_url);
+}
+
+allUser():Observable<any>
+{
+  return this.apiService.get(this.all_users_url);
+}
+addUser(userDTO: any)
+{
+  return this.apiService.post(this.user_url,userDTO);
+}
+//get single user  data
+singleUser(userId:any)
+{return this.apiService.get(this.user_url,userId);}
+//update data of  individual user
+editUser(userId:any,userDTO:any):Observable<any>{
+  return this.apiService.put(this.user_url+userId,userDTO);
+}
+
+///de;ete user
+deleteUser(userId:any){
+  return this.apiService.delete(this.user_url+userId);
+}
+
 }
--- src/app/shared/layouts/header/header.component.ts
+++ src/app/shared/layouts/header/header.component.ts
@@ -1,16 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+logged_in:boolean=false;
+language:string="English";
+user_role!:any;
+  constructor(private router:Router){}
+  ngOnInit(): void {
+    
+  }
+
+/*  
 switchLanguage(arg0: string) {
 throw new Error('Method not implemented.');
+}
+*/
+
+ngDoCheck()
+{
+  this.user_role=sessionStorage.getItem("role");
+ // console.log(this.user_role);
+ const user_session_id=sessionStorage.getItem("user_session_id");
+  if(user_session_id){
+    this.logged_in=true;
+  }
+}
+
+logout(){
+  sessionStorage.removeItem("user_session_id");
+  sessionStorage.removeItem("role");
+  this.router.navigateByUrl("/sign-in");
+  location.reload();  
 }
 
 }
