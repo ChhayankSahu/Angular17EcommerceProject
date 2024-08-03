b1708675e4aa9542f6743c6d4478d324991a8ba6
ChhayankSahu <sahu.chhayank5@gmail.com>
2024-08-04 03:48:58 +0530

    customer service done

--- src/app/customer/services/customer.service.ts
+++ src/app/customer/services/customer.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../../core/service/api.service';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
+public user_url="http://localhost:3000/user";
+public product_url="http://localhost:3000/products/";
+public order_url="http://localhost:3000/orders/";
 
-  constructor() { }
+private single_product_id=new BehaviorSubject(null);
+currentProduct=this.single_product_id.asObservable();
+  constructor(private apiservice:ApiService) { }
+
+allProduct():Observable<any>
+{
+  return this.apiservice.get(this.product_url);
+}
+quickBuyProduct(prodcut_id:any)
+{
+  this.single_product_id.next(prodcut_id);
+}
+individualProduct(id:any)
+{
+  return this.apiservice.get(this.product_url+id);
+}
+userDetail(id:any)
+{
+  return this.apiservice.get(this.user_url+"/"+id);
+}
+
+insertNewOrder(order_dto:any)
+{
+  return this.apiservice.post(this.order_url,order_dto);
+}
+orderDashboardData ():Observable<any>
+{
+return this.apiservice.get(this.order_url);
+}
+productDashboardData():Observable<any>{
+  return this.apiservice.get(this.product_url);
+}
+
+
 }
