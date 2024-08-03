edce07856f273786ec64c9fcc355b1f0edb3a098
ChhayankSahu <sahu.chhayank5@gmail.com>
2024-08-04 03:45:19 +0530

    buyerDashboard component done

--- src/app/customer/buyer/buyer-dashboard/buyer-dashboard.component.ts
+++ src/app/customer/buyer/buyer-dashboard/buyer-dashboard.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { CustomerService } from '../../services/customer.service';
 
 @Component({
   selector: 'app-buyer-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterLink],
   templateUrl: './buyer-dashboard.component.html',
   styleUrl: './buyer-dashboard.component.css'
 })
-export class BuyerDashboardComponent {
+export class BuyerDashboardComponent implements OnInit{
+all_products:any;
+show_checkout_flag:boolean=false;
+  constructor(private router:Router,private customerService:CustomerService)
+  {
 
+  }
+  
+  ngOnInit(): void {
+    this.getAllProduct();
+  }
+getAllProduct()
+{
+  this.customerService.allProduct().subscribe(
+    data=>{this.all_products=data;
+      console.log(this.all_products);
+    },error=>{console.log("my error",error);}
+  );
+}
+buyProduct(id:number)
+{
+this.show_checkout_flag=true;
+this.customerService.quickBuyProduct(id);
+this.router.navigateByUrl("/checkout");
+}
+
+addToCart()
+{
+  alert("this is showcase for addToCart method");
+}
 }
